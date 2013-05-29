0d2cb549759466e1ce7a98e171360190dcca52a5
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-05-29 18:39:32 +0530

    amir, gopal | #87 | created generic mapper to map from key value to object, test

---
+++ care-reporting-bundle/src/main/java/org/motechproject/care.reporting/mapper/GenericMapper.java
@@ -0,0 +1,40 @@
+package org.motechproject.care.reporting.mapper;
+
+
+import org.motechproject.care.reporting.utils.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+
+public class GenericMapper {
+    protected Logger logger = LoggerFactory.getLogger("commcare-reporting-mapper");
+
+    public <T> T map(Map<String, String> keyStore, Class<T> type) {
+        T newInstance;
+        try {
+            newInstance = type.newInstance();
+        } catch (Exception ex) {
+            logger.warn("Exception thrown when creating new instance" + ex);
+            return null;
+        }
+
+        return map(keyStore, newInstance);
+    }
+
+    public <T> T map(Map<String, String> keyStore, T typeInstance) {
+        for (Map.Entry<String, String> field : keyStore.entrySet()) {
+            String key = field.getKey();
+            String value = field.getValue();
+
+            logger.info("Setting " + value + " to " + key);
+            System.out.println("Setting " + value + " to " + key);
+            ObjectUtils.set(typeInstance, key, value);
+        }
+
+        return typeInstance;
+    }
+}
+
+
