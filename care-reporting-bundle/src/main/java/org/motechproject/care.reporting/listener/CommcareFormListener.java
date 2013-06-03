6b7e66c96c33bc07610b6db532af8b9e8c5880a2
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-06-03 11:28:41 +0530

    Gopal | #87 | CommcareFormListener to listen the forms event.

--- care-reporting-bundle/src/main/java/org/motechproject/care.reporting/listener/CommcareFormListener.java
+++ care-reporting-bundle/src/main/java/org/motechproject/care.reporting/listener/CommcareFormListener.java
@@ -1,38 +1,23 @@
 package org.motechproject.care.reporting.listener;
 
+import org.motechproject.care.reporting.processors.FormProcessor;
+import org.motechproject.commcare.builder.CommcareFormBuilder;
+import org.motechproject.commcare.domain.CommcareForm;
 import org.motechproject.commcare.events.constants.EventSubjects;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.annotations.MotechListener;
-import org.motechproject.server.config.SettingsFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class CommcareFormListener {
 
     @Autowired
-    private SettingsFacade settings;
+    FormProcessor formProcessor;
 
     @MotechListener(subjects = EventSubjects.FORMS_EVENT)
-    public void handleFormEvent(MotechEvent event) {
-
-
-        // read event payload
-        Map<String, Object> eventParams = event.getParameters();
-        String phoneNumber = (String) eventParams.get("phone_number");
-        String message = (String) eventParams.get("message_for_user");
-
-        // read your module's configuration
-        Long repeatInterval = Long.valueOf(settings.getProperty("repeat_interval_in_millis"));
-        Integer retries = Integer.valueOf(settings.getProperty("number_of_retries"));
-
-        // do something, maybe schedule a job to send an sms at a particular time
-        HashMap<String, Object> jobPayload = new HashMap<>();
-        jobPayload.put("message", message);
-        jobPayload.put("number", phoneNumber);
-
+    public void handleEvent(MotechEvent event) {
+        CommcareForm form = new CommcareFormBuilder().buildFrom(event);
+        formProcessor.process(form);
     }
 }
