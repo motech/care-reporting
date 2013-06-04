d252efd86053482aa24ba044fc919c3df7fb42d2
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-06-04 14:28:33 +0530

    amir, gopal | #87 | Configuration Changes.

--- care-reporting-bundle/src/test/java/org/motechproject/care/reporting/repository/SpringIntegrationTest.java
+++ care-reporting-bundle/src/test/java/org/motechproject/care/reporting/repository/SpringIntegrationTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationCommcareReportsContext.xml")
+@ContextConfiguration("classpath:META-INF/motech/applicationContext.xml")
 @Transactional
 public abstract class SpringIntegrationTest {
 
