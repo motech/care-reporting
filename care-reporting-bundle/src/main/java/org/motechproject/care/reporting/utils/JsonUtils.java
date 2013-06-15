4fab8b6f3d46fdfead1828c7c1ac4ca99cc6d40a
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-06-15 14:38:50 +0530

    ashok, gopal | #145 | Modified JsonUtils to parse InputStream.

--- care-reporting-bundle/src/main/java/org/motechproject/care/reporting/utils/JsonUtils.java
+++ care-reporting-bundle/src/main/java/org/motechproject/care/reporting/utils/JsonUtils.java
@@ -1,18 +1,40 @@
 package org.motechproject.care.reporting.utils;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.motechproject.care.reporting.model.MappingEntity;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public class JsonUtils {
 
-    public static List<MappingEntity> parseJson(String json){
+    public static List<MappingEntity> parseStreams(List<InputStream> streams) {
+        List<MappingEntity> mappingEntities = new ArrayList<>();
+
+        for (InputStream stream : streams) {
+            mappingEntities.addAll(parseStream(stream));
+        }
+
+        return mappingEntities;
+    }
+
+    private static List<MappingEntity> parseStream(InputStream stream) {
+        final String jsonContent;
+        try {
+            jsonContent = IOUtils.toString(stream);
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing mapping file", e);
+        }
+
+        return parseJson(jsonContent);
+    }
+
+    private static List<MappingEntity> parseJson(String json) {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
@@ -22,16 +44,4 @@ public class JsonUtils {
         }
     }
 
-    public static String readFile(String filePath){
-        try {
-            return FileUtils.readFileToString(new File(filePath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static List<MappingEntity> parseFile(String filePath){
-        final String json = readFile(filePath);
-        return parseJson(json);
-    }
 }
--- care-reporting-bundle/src/test/java/org/motechproject/care/reporting/utils/JsonUtilsTest.java
+++ care-reporting-bundle/src/test/java/org/motechproject/care/reporting/utils/JsonUtilsTest.java
@@ -5,6 +5,10 @@ import org.motechproject.care.reporting.model.MappingEntity;
 import org.motechproject.care.reporting.parser.*;
 import org.unitils.reflectionassert.ReflectionAssert;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,30 +19,8 @@ import static junit.framework.Assert.assertNotNull;
 public class JsonUtilsTest {
 
     @Test
-    public void shouldReadJson(){
-
-        String inputJson = "["
-         +"{\""
-         +   "identifier\": \"myId\",\""
-         +       "version\": \"myVersion\",\""
-         +       "segment\": \"mySegment\",\""
-         +       "infoParser\": {"
-         +   "\"convertToCamelCase\": false,"
-         +           "\"restrictedElements\": [\"case\", \"child_info\"],"
-         +   "\"keyConversionMap\": {"
-         +       "\"instance_ID\": \"instanceID\""
-         +   "}"
-         +"}"
-         +"}"
-         +"]";
-
-        final List<MappingEntity> mappingEntities = JsonUtils.parseJson(inputJson);
-
-
-        assertEquals(1, mappingEntities.size());
-
-        MappingEntity entity = mappingEntities.get(0);
-
+    public void shouldParseGivenInputStreamsToMappingEntities() {
+        InputStream stream = new ByteArrayInputStream(getSampleJson().getBytes());
         InfoParser expectedInfoParser = new InfoParserImpl();
         expectedInfoParser.setConvertToCamelCase(false);
         expectedInfoParser.setRestrictedElements(asList("case", "child_info"));
@@ -47,10 +29,38 @@ public class JsonUtilsTest {
         }};
         expectedInfoParser.setKeyConversionMap(keyConversionMap);
 
+        final List<MappingEntity> mappingEntities = JsonUtils.parseStreams(asList(stream));
+
+        assertEquals(1, mappingEntities.size());
+        MappingEntity entity = mappingEntities.get(0);
         assertEquals("myId", entity.getIdentifier());
         assertEquals("myVersion", entity.getVersion());
         assertEquals("mySegment", entity.getSegment());
         assertNotNull(entity.getInfoParser());
         ReflectionAssert.assertReflectionEquals(expectedInfoParser, entity.getInfoParser());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionForNullStreamOrForIOErrors() {
+        InputStream inputStream = null;
+
+        JsonUtils.parseStreams(asList(inputStream));
+    }
+
+    private String getSampleJson(){
+        return "["
+                +"{\""
+                +   "identifier\": \"myId\",\""
+                +       "version\": \"myVersion\",\""
+                +       "segment\": \"mySegment\",\""
+                +       "infoParser\": {"
+                +   "\"convertToCamelCase\": false,"
+                +           "\"restrictedElements\": [\"case\", \"child_info\"],"
+                +   "\"keyConversionMap\": {"
+                +       "\"instance_ID\": \"instanceID\""
+                +   "}"
+                +"}"
+                +"}"
+                +"]";
     }
 }
