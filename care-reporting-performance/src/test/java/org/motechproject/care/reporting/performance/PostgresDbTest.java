748c806ce7ff318082762fba64a226d887f33515
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-06-18 11:47:31 +0530

    amir, gopal | #91 | renamed benerator xml and test class names.

--- care-reporting-performance/src/test/java/org/motechproject/care/reporting/performance/PostgresDbTest.java
+++ care-reporting-performance/src/test/java/org/motechproject/care/reporting/performance/PostgresDbTest.java
@@ -6,13 +6,15 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-public class FormTest {
+public class PostgresDbTest {
 
     @Test
-    public void createForms() throws IOException {
+    public void populateDb() throws IOException {
         long startTime = System.currentTimeMillis();
-        Benerator.main(new String[]{"form_generation.ben.xml"});
+        Benerator.main(new String[]{"postgres_generation.ben.xml"});
         long endTime = System.currentTimeMillis();
         System.out.println("Timetaken: " + (endTime - startTime)/1000 + " seconds");
     }
 }
+
+
