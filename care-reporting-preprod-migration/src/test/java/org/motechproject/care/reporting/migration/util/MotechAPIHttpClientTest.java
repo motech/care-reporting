e27881fab4c93f404a8f5a74162568e3cc419986
vishnu karthik <karthikl@thoughtworks.com>
2013-06-24 11:16:33 +0530

    Vishnu, Ashish | #82 | Using DefaultHttpClient

--- care-reporting-preprod-migration/src/test/java/org/motechproject/care/reporting/migration/util/MotechAPIHttpClientTest.java
+++ care-reporting-preprod-migration/src/test/java/org/motechproject/care/reporting/migration/util/MotechAPIHttpClientTest.java
@@ -1,24 +1,15 @@
 package org.motechproject.care.reporting.migration.util;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.http.client.HttpClient;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
-import java.io.IOException;
 import java.util.Properties;
 
 import static junit.framework.Assert.assertEquals;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -36,53 +27,25 @@ public class MotechAPIHttpClientTest {
     }
 
     @Test
-    public void shouldPostFormToMotechFormEndpoint() throws IOException {
-        String form = "form";
+    public void shouldPostToFormUrl() {
         when(platformProperties.getProperty("app.url")).thenReturn("localhost");
-        when(platformProperties.getProperty("app.form.endpoint")).thenReturn("motech/forms");
+        when(platformProperties.getProperty("app.form.endpoint")).thenReturn("forms");
+
         MotechAPIHttpClient motechAPIHttpClient = new MotechAPIHttpClient(httpClient, platformProperties);
 
-        motechAPIHttpClient.postForm(form);
-
-        ArgumentCaptor<PostMethod> methodCaptor = ArgumentCaptor.forClass(PostMethod.class);
-        verify(httpClient).executeMethod(methodCaptor.capture());
-        PostMethod postmethod = methodCaptor.getValue();
-        assertEquals("localhost/motech/forms", postmethod.getPath());
-        assertThat(postmethod.getRequestEntity(), instanceOf(StringRequestEntity.class));
-        assertEquals(form, ((StringRequestEntity) postmethod.getRequestEntity()).getContent());
-        assertEquals("text/xml; charset=UTF-8", postmethod.getRequestEntity().getContentType());
-        assertEquals("UTF-8", postmethod.getRequestCharSet());
+        String formUpdateUrl = motechAPIHttpClient.getFormUpdateUrl();
+        assertEquals("localhost/forms", formUpdateUrl);
     }
 
     @Test
-    public void shouldPostCaseToMotechCaseEndpoint() throws IOException {
-        String aCase = "case";
+    public void shouldPostToCaseUrl() {
+
         when(platformProperties.getProperty("app.url")).thenReturn("localhost");
-        when(platformProperties.getProperty("app.case.endpoint")).thenReturn("motech/cases");
-        MotechAPIHttpClient motechAPIHttpClient = new MotechAPIHttpClient(httpClient, platformProperties);
-
-        motechAPIHttpClient.postCase(aCase);
-
-        ArgumentCaptor<PostMethod> methodCaptor = ArgumentCaptor.forClass(PostMethod.class);
-        verify(httpClient).executeMethod(methodCaptor.capture());
-        PostMethod postmethod = methodCaptor.getValue();
-        assertEquals("localhost/motech/cases", postmethod.getPath());
-        assertThat(postmethod.getRequestEntity(), instanceOf(StringRequestEntity.class));
-        assertEquals(aCase, ((StringRequestEntity) postmethod.getRequestEntity()).getContent());
-        assertEquals("text/xml; charset=UTF-8", postmethod.getRequestEntity().getContentType());
-        assertEquals("UTF-8", postmethod.getRequestCharSet());
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenPostFails() throws IOException {
-        expectedException.expect(RuntimeException.class);
-        expectedException.expectMessage("post failed");
-
-        doThrow(new RuntimeException("post failed")).when(httpClient).executeMethod(any(PostMethod.class));
+        when(platformProperties.getProperty("app.case.endpoint")).thenReturn("cases");
 
         MotechAPIHttpClient motechAPIHttpClient = new MotechAPIHttpClient(httpClient, platformProperties);
 
-        motechAPIHttpClient.postForm("form");
+        String caseUpdateUrl = motechAPIHttpClient.getCaseUpdateUrl();
+        assertEquals("localhost/cases", caseUpdateUrl);
     }
-
 }
