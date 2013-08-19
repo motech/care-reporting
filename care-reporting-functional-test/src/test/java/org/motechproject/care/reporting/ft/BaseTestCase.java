cf7f797b10e1f8bb7846d9c15edf728437bb6a5a
Ashish Mishra <ashishkm@thoughtworks.com>
2013-08-19 16:51:59 +0530

    Ashish|#00|Fixing functional test

--- care-reporting-functional-test/src/test/java/org/motechproject/care/reporting/ft/BaseTestCase.java
+++ care-reporting-functional-test/src/test/java/org/motechproject/care/reporting/ft/BaseTestCase.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationCareReportingFunctionTests.xml")
+@ContextConfiguration("classpath:applicationCareReportingFunctionalTests.xml")
 public abstract class BaseTestCase {
 
     @Autowired
