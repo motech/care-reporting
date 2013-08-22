33d65a5e151e782aa30e7885797d9dd6578d6010
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-08-22 16:52:06 +0530

    Gopal | #0 | Marked the ComputeFieldsJob as Transactional.

--- care-reporting-bundle/src/main/java/org/motechproject/care/reporting/job/ComputeFieldsJob.java
+++ care-reporting-bundle/src/main/java/org/motechproject/care/reporting/job/ComputeFieldsJob.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Transactional
 public class ComputeFieldsJob {
 
     Repository repository;
