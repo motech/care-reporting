76fb52cf4f8783efd3906c6295cc84052a405190
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-08-22 15:43:32 +0530

    Gopal | #0 | Enhanced the delivery offset days function to include only date and check any day greater than last modified.

--- care-reporting-migration/src/main/resources/db/migration/V1_4.sql
+++ care-reporting-migration/src/main/resources/db/migration/V1_4.sql
@@ -3,23 +3,23 @@ DECLARE
     result RECORD;
 BEGIN
 	FOR result IN Select table_name from report.domain_metadata where type = 'form' and category='mother' LOOP
-		EXECUTE 'UPDATE report.' || result.table_name || ' AS v SET delivery_offset_days = CASE WHEN mc.add IS NOT NULL THEN extract(day from (v.server_date_modified - mc.add))
-												     WHEN mc.edd IS NOT NULL THEN extract(day from (v.server_date_modified - mc.edd))
+		EXECUTE 'UPDATE report.' || result.table_name || ' AS v SET delivery_offset_days = CASE WHEN mc.add IS NOT NULL THEN extract(day from (date_trunc(''day'', v.server_date_modified) - date_trunc(''day'', mc.add)))
+												     WHEN mc.edd IS NOT NULL THEN extract(day from (date_trunc(''day'', v.server_date_modified) - date_trunc(''day'', mc.edd)))
 												     ELSE null
 												     END FROM report.mother_case mc
-												     INNER JOIN report.job_metadata md ON mc.last_modified_time between md.last_run and current_timestamp
-												     WHERE mc.id = v.case_id';
+												     INNER JOIN report.job_metadata md ON mc.last_modified_time >= md.last_run
+												     WHERE mc.id = v.case_id AND md.job_name = ''populate_delivery_offset_days''';
 	END LOOP;
 
 	FOR result IN Select table_name from report.domain_metadata where type = 'form' and category='child' LOOP
-		EXECUTE 'UPDATE report.' || result.table_name || ' AS v SET delivery_offset_days = CASE WHEN mc.add IS NOT NULL THEN extract(day from (v.server_date_modified - mc.add))
-												     WHEN mc.edd IS NOT NULL THEN extract(day from (v.server_date_modified - mc.edd))
+		EXECUTE 'UPDATE report.' || result.table_name || ' AS v SET delivery_offset_days = CASE WHEN mc.add IS NOT NULL THEN extract(day from (date_trunc(''day'', v.server_date_modified) - date_trunc(''day'', mc.add)))
+												     WHEN mc.edd IS NOT NULL THEN extract(day from (date_trunc(''day'', v.server_date_modified) - date_trunc(''day'', mc.edd)))
 												     ELSE null
 												     END 
 												     FROM report.child_case cc
 												     INNER JOIN report.mother_case mc ON cc.mother_id = mc.id
-												     INNER JOIN report.job_metadata md ON mc.last_modified_time between md.last_run and current_timestamp
-												     WHERE cc.id = v.case_id';
+												     INNER JOIN report.job_metadata md ON mc.last_modified_time >= md.last_run
+												     WHERE cc.id = v.case_id AND md.job_name = ''populate_delivery_offset_days''';
 	END LOOP;
 
 	UPDATE  report.job_metadata
