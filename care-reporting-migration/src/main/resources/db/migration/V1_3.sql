79424bd0ae7231389fe37ab4abe13f45eafa76dd
Amirdha Gopal R <amirdhagopal@gmail.com>
2013-08-22 12:11:14 +0530

    Gopal | #0 | Refactored metadata table creation script.

--- care-reporting-migration/src/main/resources/db/migration/V1_3.sql
+++ care-reporting-migration/src/main/resources/db/migration/V1_3.sql
@@ -1,15 +1,27 @@
-With domain_meta_info as (
-Select table_name, 'form' as type, 'mother' as category from information_schema.tables where table_schema = 'report' and table_name like '%_form' and table_name NOT like '%_child_%'
+CREATE TABLE report.domain_metadata
+(
+  id SERIAL PRIMARY KEY,
+  table_name varchar(255),
+  type varchar(50),
+  category varchar(50)
+);
+
+INSERT INTO report.domain_metadata(table_name, type, category)
+Select table_name, 'form', 'mother' from information_schema.tables where table_schema = 'report' and table_name like '%_form' and table_name NOT like '%_child_%'
 UNION ALL
 Select table_name, 'form', 'child' from information_schema.tables where table_schema = 'report' and table_name like '%_form' and table_name like '%_child_%'
 UNION ALL
 Select table_name, 'case', 'mother' from information_schema.tables where table_schema = 'report' and table_name like '%_case' and table_name not like 'child_%'
 UNION ALL
-Select table_name, 'case', 'child' from information_schema.tables where table_schema = 'report' and table_name like '%_case' and table_name like '%child_%'
-)
-Select row_number() over() as id, * into report.domain_metadata  from domain_meta_info;
+Select table_name, 'case', 'child' from information_schema.tables where table_schema = 'report' and table_name like '%_case' and table_name like '%child_%';
 
-With job_meta_info as (
-Select 'populate_delivery_offset_days' as job_name, current_timestamp as last_run
-)
-Select row_number() over() as id, * into report.job_metadata  from job_meta_info;
+
+CREATE TABLE report.job_metadata
+(
+  id SERIAL PRIMARY KEY,
+  job_name varchar(255),
+  last_run timestamp with time zone
+);
+
+INSERT INTO report.job_metadata(job_name, last_run)
+SELECT 'populate_delivery_offset_days', current_timestamp;
