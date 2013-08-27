536c784f6c1857166b2db53367918579c4224494
Ashish Mishra <ashishkm@thoughtworks.com>
2013-08-27 10:45:38 +0530

    Ashish|#00|Added more logging around migrator

--- care-reporting-preprod-migration/src/main/java/org/motechproject/care/reporting/migration/Migrator.java
+++ care-reporting-preprod-migration/src/main/java/org/motechproject/care/reporting/migration/Migrator.java
@@ -3,6 +3,8 @@ package org.motechproject.care.reporting.migration;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.motechproject.care.reporting.migration.service.MigrationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
@@ -12,16 +14,20 @@ public class Migrator {
 
     private MigrationService migrationService;
 
+    private static final Logger logger = LoggerFactory.getLogger(Migrator.class);
+
     @Autowired
     public Migrator(MigrationService migrationService) {
         this.migrationService = migrationService;
     }
 
     public static void main(String[] args) {
+        logger.info("Running migrator ...");
         boolean success = true;
         DateTime startTime = DateTime.now();
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationCareMigration.xml");
 
+        Exception exception = null;
         try {
             Migrator migrator = applicationContext.getBean(Migrator.class);
             MigratorArguments migratorArguments = new MigratorArguments(args);
@@ -29,17 +35,27 @@ public class Migrator {
             migrator.migrate(migratorArguments);
 
         } catch (IllegalArgumentException e) {
+            exception = e;
             System.out.println("Error: " + e.getMessage());
             System.out.println("Usage: " + MigratorArguments.usage());
+            success = false;
         } catch (RuntimeException e) {
+            exception = e;
+            System.out.print("Error: " + e.getMessage());
             System.out.println("Migration Failed");
             success = false;
-        } finally {
-            System.out.printf("Total time taken for migration: %d mins %n", new Duration(startTime, DateTime.now()).getStandardMinutes());
-            applicationContext.destroy();
         }
-        if (!success)
-            System.exit(1);
+
+        if(success) {
+            logger.info("Migration ended.");
+        } else {
+            logger.error("Exception occurred while running migrator", exception);
+            logger.error("Migration failed.");
+        }
+
+        System.out.printf("Total time taken for migration: %d mins %n", new Duration(startTime, DateTime.now()).getStandardMinutes());
+        applicationContext.destroy();
+        System.exit(success ? 0 : 1);
     }
 
     public void migrate(MigratorArguments migratorArguments) {
